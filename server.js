4624c65dd0df5fc6ff35fd68bd4fe22f696cddfb
Miguel Davila Jr <migueldavilajr@gmail.com>
2022-01-11 18:28:13 -0800

    Added index js to routes and routes/api

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
+const express = require('path');
+const express = require('express');
+const session = require('express-session');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', engine());
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.engine('handlebars', hbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
